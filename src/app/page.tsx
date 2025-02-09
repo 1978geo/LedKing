0dd523b522b2579571beafae7df5739aaab34872
Zlatko <iliev.zlatko@gmail.com>
2025-02-09 14:49:20 +0200

    initial setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-      <Button variant='secondary'>Click me</Button>
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      <Button>Click me</Button>
     </div>
   )
 }
