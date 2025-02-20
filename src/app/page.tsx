9931363360e4b7b83295c474d0d9ab47393dfbb8
Zlatko <iliev.zlatko@gmail.com>
2025-02-20 13:19:38 +0200

    fix the path to the video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
               }}
             ></iframe> */}
             <MediaPlayer
-              source='LEDKing.mp4'
+              source='LEDKING.mp4'
               controls
               muted
               autoPlay
