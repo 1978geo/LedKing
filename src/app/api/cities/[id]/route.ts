408fe3c4e4980c21af8bb5e611d029fbce56c05b
Zlatko <iliev.zlatko@gmail.com>
2025-02-21 11:08:50 +0200

    fix build errors

--- src/app/api/cities/[id]/route.ts
+++ src/app/api/cities/[id]/route.ts
@@ -1,11 +1,14 @@
 import { findById } from '@/db/db.repository'
 import { CityEntity } from '@/types/City.type'
-import { NextResponse } from 'next/server'
-import { NextApiRequest } from 'next'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(_: NextApiRequest, context: any) {
+type Context = {
+  params: Promise<{ id: string }>
+}
+
+export async function GET(_: NextRequest, context: Context) {
   const { id } = await context.params
-  const city = await findById<CityEntity>('cities', id as string)
+  const city = await findById<CityEntity>('cities', id)
   return NextResponse.json(city, {
     headers: {
       'Content-Type': 'application/json',
