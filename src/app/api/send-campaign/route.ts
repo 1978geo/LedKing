dfe5de977a3fc07fe34902de61d39aa87a99f624
Zlatko <iliev.zlatko@gmail.com>
2025-03-21 10:09:16 +0200

    fix api key

--- src/app/api/send-campaign/route.ts
+++ src/app/api/send-campaign/route.ts
@@ -5,7 +5,7 @@ import {
 import { NextRequest } from 'next/server'
 import { Resend } from 'resend'
 
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY)
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(req: NextRequest) {
   const body: LEDCampaignEmailProps = await req.json()
