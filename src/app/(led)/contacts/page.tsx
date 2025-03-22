0fe6e107b96d9aac69e3976ef5f6a642e1c81f99
Zlatko <iliev.zlatko@gmail.com>
2025-03-22 13:59:55 +0200

    add address fields

--- src/app/(led)/contacts/page.tsx
+++ src/app/(led)/contacts/page.tsx
@@ -3,9 +3,29 @@ import LedContactsForm from '@/components/forms/led-contacts.form'
 export default function ContactsPage() {
   return (
     <div className='flex flex-col flex-1 h-full w-full max-w-7xl mx-auto'>
-      <h2 className='text-center font-semibold text-2xl lg:text-3xl my-10'>
-        Контакти
-      </h2>
+      <div className='flex justify-between flex-col lg:flex-row w-full px-4 my-14'>
+        <h2 className='text-center font-semibold text-2xl lg:text-4xl'>
+          Контакти
+        </h2>
+        <div className='flex flex-col gap-y-1 text-xs text-slate-500'>
+          <div className='grid grid-cols-[80px_1fr]'>
+            <h3 className='font-semibold uppercase'>Email:</h3>
+            <p>
+              <a href='mailto:office@ledking.bg'>office@ledking.bg</a>
+            </p>
+          </div>
+          <div className='grid grid-cols-[80px_1fr]'>
+            <h3 className='font-semibold uppercase'>Телефон:</h3>
+            <a href='tel:+359897937355'>(+359) 897 93 73 55</a>
+          </div>
+          <div className='grid grid-cols-[80px_1fr]'>
+            <h3 className='font-semibold uppercase'>Адрес:</h3>
+            <p className='max-w-[160px]'>
+              КК. "СЛЪНЧЕВ БРЯГ" 8240, алея "Кубан" сграда "Кая"
+            </p>
+          </div>
+        </div>
+      </div>
       <LedContactsForm />
     </div>
   )
