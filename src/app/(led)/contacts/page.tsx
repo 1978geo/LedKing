273e0284465608d9d49ab9e5e0d1de9fd1ac7174
Zlatko <iliev.zlatko@gmail.com>
2025-03-22 14:06:14 +0200

    fix the contacts text

--- src/app/(led)/contacts/page.tsx
+++ src/app/(led)/contacts/page.tsx
@@ -3,11 +3,11 @@ import LedContactsForm from '@/components/forms/led-contacts.form'
 export default function ContactsPage() {
   return (
     <div className='flex flex-col flex-1 h-full w-full max-w-7xl mx-auto'>
-      <div className='flex justify-between flex-col lg:flex-row w-full px-4 my-14'>
+      <div className='flex justify-between flex-col lg:flex-row w-full px-4 my-10 lg:my-14'>
         <h2 className='text-center font-semibold text-2xl lg:text-4xl'>
           Контакти
         </h2>
-        <div className='flex flex-col gap-y-1 text-xs text-slate-500'>
+        <div className='flex flex-col gap-y-1 text-xs text-primary-purple mt-6 mx-auto lg:mt-0 lg:mx-0'>
           <div className='grid grid-cols-[80px_1fr]'>
             <h3 className='font-semibold uppercase'>Email:</h3>
             <p>
@@ -21,7 +21,8 @@ export default function ContactsPage() {
           <div className='grid grid-cols-[80px_1fr]'>
             <h3 className='font-semibold uppercase'>Адрес:</h3>
             <p className='max-w-[160px]'>
-              КК. "СЛЪНЧЕВ БРЯГ" 8240, алея "Кубан" сграда "Кая"
+              КК. &quot;СЛЪНЧЕВ БРЯГ&quot; 8240, алея &quot;Кубан&quot; сграда
+              &quot;Кая&quot;
             </p>
           </div>
         </div>
