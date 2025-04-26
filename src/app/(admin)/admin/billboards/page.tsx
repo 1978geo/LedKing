e436cc3a059cc7d5754244deaf16ee6bc57880e8
Zlatko <iliev.zlatko@gmail.com>
2025-04-26 18:39:34 +0300

    add title to billboards page

--- src/app/(admin)/admin/billboards/page.tsx
+++ src/app/(admin)/admin/billboards/page.tsx
@@ -2,6 +2,7 @@ import { getBillboards } from '@/actions/billboards'
 import { requireAuth } from '@/lib/require-auth.server'
 import { BillboardsTable } from './_components/billboards-table'
 import { getCitiesList } from '@/actions/cities'
+import { PageTitle } from '@/components/admin/page-title'
 
 export default async function BillbaordsPage() {
   await requireAuth()
@@ -9,9 +10,16 @@ export default async function BillbaordsPage() {
   const cities = await getCitiesList()
 
   return (
-    <BillboardsTable
-      data={billboards}
-      cities={cities}
-    />
+    <div className='flex flex-col w-full gap-y-6'>
+      <PageTitle
+        title='Billboards'
+        subtitle='Manage billboards for your locations'
+      />
+
+      <BillboardsTable
+        data={billboards}
+        cities={cities}
+      />
+    </div>
   )
 }
